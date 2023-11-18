8db6c2eb2b46a64a7dfc0160aa0b2c021a2932d0
Rishi-Dave <rishipdave@gmail.com>
2023-11-18 08:52:08 -0800

    Initial Commit

---
+++ emergency_widget/emergency_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  emergency_widgetApp.swift
+//  emergency_widget
+//
+//  Created by Rishi Dave on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct emergency_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
